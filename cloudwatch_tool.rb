92579decb736c3e6563348ae06c85933eb8709f9
initialjk <initialjk@gmail.com>
2015-06-23 11:27:21 +0900

    Add actual function to register new alarm

--- cloudwatch_tool.rb
+++ cloudwatch_tool.rb
@@ -27,12 +27,41 @@ class AwsClient
     list
   end
 
+  METRIC_ALARM_PARAMS = %i[alarm_description actions_enabled statistic period unit evaluation_periods threshold comparison_operator]
+  def put_metric_alarm(metric, config)
+    param = {
+        alarm_name: '%s-%s-%s' % [config[:alarm_prefix], metric[:resource_name], metric[:metric]],
+        ok_actions: [config[:ok_sns_topic]],
+        alarm_actions: [config[:alarm_sns_topic]],
+        insufficient_data_actions: [config[:insufficient_sns_topic]],
+        metric_name: metric[:metric],
+        namespace: metric[:namespace],
+        dimensions: metric[:dimensions],
+    }
+
+    resp = @cloudwatch.put_metric_alarm METRIC_ALARM_PARAMS.inject(param) {|h, n| h.merge(n => config[n]) }
+  end
+
   def retrieve_ec2_tags(tags)
     puts 'Retrieve follow tags from AWS ... ' + tags.to_s
     tags = @ec2.describe_tags(:filters=>[{:name=>'key', :values=>tags}]).data.tags
     puts "#{tags.size} records are received."
     Hash[tags.map {|x| [x.resource_id, x]}]
   end
+end
+
+class String  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red; colorize(31); end
+  def green; colorize(32); end
+  def yellow; colorize(33); end
+  def blue; colorize(34); end
+  def pink; colorize(35); end
+  def light_blue; colorize(36); end
+  def light_grey; colorize(37); end
 end
 
 class CommandShell
@@ -69,7 +98,7 @@ class CommandShell
   def command_h
   end
 
-  def put_status
+  def put_status(indent: '', with_detail: false)
   end
 
   def done
@@ -119,8 +148,16 @@ class SelectCommandShell < CommandShell
     (item.include? :name)? item[:name]: item[:key]
   end
 
-  def put_status(with_detail)
-    puts "Selected items: #{@selection.to_a}"
+  def value_of(key)
+    key
+  end
+
+  def put_status(indent:'', with_detail:false)
+    if with_detail
+      puts @selection.map {|key| indent + format(value_of(key))}
+    else
+      puts indent + @selection.map {|key| format_simple(value_of(key))}.join(', ')
+    end
   end
 
   def prompt
@@ -130,15 +167,15 @@ class SelectCommandShell < CommandShell
 
     items.each_with_index do | x, i |
       @display_index[i] = x[:key]
-      s = (@selection.include? x[:key])? '*': ' '
-      puts '%-1s[%d]: %s' % [s, i, format(x)]
+      s = (@selection.include? x[:key])? "\e[32m*": ' '
+      puts '%-1s[%d]: %s%s' % [s, i, format(x), "\e[0m"]
     end
     print '(separate with comma, enter a space to finish) > '
 
     if (input = $stdin.gets.chomp.strip).empty?
       done # ask_done when it's important
     else
-      input.split(',').each {|x| toggle_selection x}
+      input.split( ).each {|x| toggle_selection x}
     end    
   end
 
@@ -153,12 +190,18 @@ class PropertyCommandShell < CommandShell
   def initialize
     command_clear
   end
-  
+
+  def command_clear
+    @config = properties.inject({}) {|h, p| h.merge(p[:name] => p[:default])}
+  end
+
+  attr_reader :config
+
   def prompt
     puts help_message
 
     properties.each_with_index do |x, i|
-      puts '[%d]: %s = %s' % [i, x[:name], @instance[x[:name]]]
+      puts '[%d]: %s = %s' % [i, x[:name], @config[x[:name]]]
     end
     print '(select number or property name to set. "/clear" to reset all to defaut.) > '
 
@@ -172,14 +215,13 @@ class PropertyCommandShell < CommandShell
   end
 
   def edit_property(property_key)
-    prop = get_property(property_key)
-    unless prop && (property_name = prop[:name])
+    unless (prop = get_property property_key) && (property_name = prop[:name])
       puts "Invalid property index or name '#{property_key}'"
       return
     end
 
-    puts "Enter a value for property '#{property_name}'. (#{prop[:desc]})"
-    puts "Current value is '#{@instance[property_name]}'"
+    puts '',"Enter a value for property '#{property_name}'. (#{prop[:desc]})"
+    puts "Current value is '#{@config[property_name]}'"
     if prop.include? :values
       puts 'Available values'
       prop[:values].each_with_index {|x,i| puts '  [%d]: %s' % [i, x]}
@@ -190,36 +232,46 @@ class PropertyCommandShell < CommandShell
     end until set_property(prop, $stdin.gets.chomp.strip)
   end
 
-  def set_property(prop, value)
-    return @instance[prop[:name]] = value unless prop.include? :values
 
-    valid_value = (/^\d+$/.match value)? prop[:values][value.to_i]: prop[:values].find {|v| v.casecmp(value)}
-    if valid_value
-      (@instance[prop[:name]] = valid_value) || ''
+  def parse_value(prop, input)
+    if prop.include? :values
+      value = prop[:values].find{|v| v.casecmp(input).zero?} || (/^\d+$/.match(input) && prop[:values][input.to_i])
     else
-      puts "Prop #{prop[:name]} can't be '#{value}'."
+      value = input
     end
+    return nil if value.nil?
+    
+    case prop[:type]
+    when :int
+      return value.to_i if /^\d+$/.match(value)
+      puts "Input value #{input} is not integer"
+    when :float
+      return value.to_f if /^\d+([.]\d+)?$/.match(value)
+      puts "Input value #{input} is not floating point number"
+    else # do nothing
+        value
+    end
+  end
+
+  def set_property(prop, input)
+    return puts "Prop #{prop[:name]} can't be '#{input}'." if (value = parse_value(prop, input)) .nil?
+    @config[prop[:name]] = value
   end
 
   def get_property(keyword)
     return properties[keyword.to_i] if (/^\d+$/.match keyword)
-    properties.select {|p| (name = p[:name]) && name.casecmp(keyword).zero?}.first
-  end
-
-  def command_clear
-    @instance = properties.inject({}) {|h, p| h.merge(p[:name] => p[:default])}
+    properties.select {|p| (name = p[:name]) && name.to_s.casecmp(keyword).zero?}.first
   end
 
   def help_message
   end
 
-  def put_status(with_detail=true)
+  def put_status(indent: '', with_detail: true)
     properties.each do |x|
-      if with_detail
-        puts '%s = %s (default: %s)' % [x[:name], @instance[x[:name]], x[:default]]
-      else
-        puts '%s = %s' % [x[:name], @instance[x[:name]]]
-      end
+      print indent
+      print '%s = %s' % [x[:name], @config[x[:name]]]
+      print " (default: #{x[:default]})" if with_detail
+      puts
     end
   end
 end
@@ -246,23 +298,25 @@ You can move to next step by enter a space when you are done.
 end
 
 class InstanceSelectCommandShell < SelectCommandShell
-  REQUIRED_RESOURCES = %w(VolumeId InstanceId)
+  def to_tag(dimension)
+    (tag = @tags[dimension.value])? tag.value: dimension.name + '=' + dimension.value
+  end
 
   def initialize(metrics)
     super()
 
     @tags = AwsClient.instance.retrieve_ec2_tags ['Name']
-    @items = metrics.select{|x| x.dimensions}.map do |m|
-      m.dimensions.select{|x| REQUIRED_RESOURCES.include? x.name}.map do |d|
-        {
-          :key => '%s:%s' % [d.value, m.metric_name],
-          :name => d.value,
-          :resource_name => @tags.include?(d.value)? @tags[d.value].value: nil,
-          :name_space => m.namespace,
-          :metric => m.metric_name,
-        }
-      end
-    end.flatten 1
+    @items = metrics.map do |m|
+      dvs = m.dimensions.nil?? '': (m.dimensions.map {|d| d.value}.join(', '))
+      {
+        :key => '%s:%s' % [dvs, m.metric_name],
+        :name => dvs,
+        :resource_name => m.dimensions.map(&method(:to_tag)).join(', '),
+        :namespace => m.namespace,
+        :metric => m.metric_name,
+        :dimensions => m.dimensions,
+      }
+    end
     @item_map = Hash[@items.map {|x| [x[:key], x]}]
   
     @filter = Set.new
@@ -284,49 +338,54 @@ It will shows instances with these metrics : #{@filter.to_a}
     '%s(%s)' % [item[:name], item[:resource_name]]
   end
 
-  def put_status(with_detail)
-    if with_detail
-      puts 'Selected instances {{{', @selection.map{|key| format @item_map[key]}, '}}}'
-    else
-      puts 'Selected instances: ' + @selection.map{|key| format_simple @item_map[key]}.join(', ')
-    end
+  def put_status(indent:'', with_detail:false)
+    puts indent + 'Selected instances:'
+    super(indent:indent + '  ', with_detail:with_detail)
   end
 
   def items
     @items.select {|x| @filter.include? x[:metric]}
   end
 
+  def value_of(key)
+    @item_map[key]
+  end
+
   def filter=(filter)
     @filter = filter
     @selection.select! {|key| (item = @item_map[key]) && filter.include?(item[:metric])}
+  end
+
+  def selected_metrics
+    @items.select {|x| @selection.include? x[:key]}
   end
 end
 
 class AlarmPropertyCommandShell < PropertyCommandShell
   PROPERTIES =
     [
-     { name: 'alarm_prefix', desc: %q[The descriptive prefix for the alarm. This combination of this prefix and resource and metric name must be unique within the user's AWS account.], default: Time.new.strftime('%m%d-%H%M%S:') },
-     { name: 'alarm_description', desc: %q[The description for the alarm.], default: nil },
-     { name: 'actions_enabled', desc: %q[Indicates whether or not actions should be executed during any changes to the alarm's state.], values: [true, false], default: true },
-     { name: 'ok_sns_topic', desc: %q[The list of sns topic to execute when this alarm transitions into an OK state from any other state.], values: ['grape-server-developers'], default: 'grape-server-developers' },
-     { name: 'alarm_sns_topic', desc: %q[The list of sns topic to execute when this alarm transitions into an ALARM state from any other state.], values: ['grape-server-developers'], default: 'grape-server-developers' },
-     { name: 'insufficient_sns_topic', desc: %q[The list of sns topic to execute when this alarm transitions into an INSUFFICIENT_DATA state from any other state.], values: ['grape-server-developers'], default: 'grape-server-developers' },
-     { name: 'statistic', desc: %q[The statistic to apply to the alarm's associated metric.], values: %w[SampleCount Average Sum Minimum Maximum], default: 'Average' },
-     { name: 'period', desc: %q[The period in seconds over which the specified statistic is applied.], default: 1},
-     { name: 'unit', desc: %q[The unit for the alarm's associated metric.], values: %w[Seconds Microseconds Milliseconds Bytes Kilobytes Megabytes Gigabytes Terabytes Bits Kilobits Megabits Gigabits Terabits Percent Count Bytes/Second Kilobytes/Second Megabytes/Second Gigabytes/Second Terabytes/Second Bits/Second Kilobits/Second Megabits/Second Gigabits/Second Terabits/Second Count/Second None], default: 'Count'},
-     { name: 'evaluation_periods', desc: %q[The number of periods over which data is compared to the specified threshold.], default: 1 },
-     { name: 'threshold', desc: %q[The value against which the specified statistic is compared.], default: 0.0 },
-     { name: 'comparison_operator', desc: %q[The arithmetic operation to use when comparing the specified Statistic and Threshold. The specified Statistic value is used as the first operand.], values: %w[GreaterThanOrEqualToThreshold GreaterThanThreshold LessThanThreshold LessThanOrEqualToThreshold], default: 'GreaterThanOrEqualToThreshold' },
+     { name: :alarm_prefix, desc: %q[The descriptive prefix for the alarm. This combination of this prefix and resource and metric name must be unique within the user's AWS account.], default: Time.new.strftime('%m%d-%H%M%S') },
+     { name: :alarm_description, desc: %q[The description for the alarm.], default: Time.new.strftime('Added at %m%d-%H%M%S') },
+     { name: :actions_enabled, desc: %q[Indicates whether or not actions should be executed during any changes to the alarm's state.], values: [true, false], default: true },
+     { name: :ok_sns_topic, desc: %q[The list of sns topic to execute when this alarm transitions into an OK state from any other state.], values: ['arn:aws:sns:ap-northeast-1:871396498765:grape-server-developers'], default: 'arn:aws:sns:ap-northeast-1:871396498765:grape-server-developers' },
+     { name: :alarm_sns_topic, desc: %q[The list of sns topic to execute when this alarm transitions into an ALARM state from any other state.], values: ['arn:aws:sns:ap-northeast-1:871396498765:grape-server-developers'], default: 'arn:aws:sns:ap-northeast-1:871396498765:grape-server-developers' },
+     { name: :insufficient_sns_topic, desc: %q[The list of sns topic to execute when this alarm transitions into an INSUFFICIENT_DATA state from any other state.], values: ['arn:aws:sns:ap-northeast-1:871396498765:grape-server-developers'], default: 'arn:aws:sns:ap-northeast-1:871396498765:grape-server-developers' },
+     { name: :statistic, desc: %q[The statistic to apply to the alarm's associated metric.], values: %w[SampleCount Average Sum Minimum Maximum], default: 'Average' },
+     { name: :period, desc: %q[The period in seconds over which the specified statistic is applied.], values: %w[60 120 180 240 300 360 420 480 540 600], type: :int, default: 60},
+     { name: :unit, desc: %q[The unit for the alarm's associated metric.], values: %w[Seconds Microseconds Milliseconds Bytes Kilobytes Megabytes Gigabytes Terabytes Bits Kilobits Megabits Gigabits Terabits Percent Count Bytes/Second Kilobytes/Second Megabytes/Second Gigabytes/Second Terabytes/Second Bits/Second Kilobits/Second Megabits/Second Gigabits/Second Terabits/Second Count/Second None], default: 'Count'},
+     { name: :evaluation_periods, desc: %q[The number of periods over which data is compared to the specified threshold.], type: :int, default: 3 },
+     { name: :threshold, desc: %q[The value against which the specified statistic is compared.], type: :float, default: 0.0 },
+     { name: :comparison_operator, desc: %q[The arithmetic operation to use when comparing the specified Statistic and Threshold. The specified Statistic value is used as the first operand.], values: %w[GreaterThanOrEqualToThreshold GreaterThanThreshold LessThanThreshold LessThanOrEqualToThreshold], default: 'GreaterThanOrEqualToThreshold' },
     ]
 
   def properties
     PROPERTIES
   end
 
-  def put_status(with_detail=true)
-    puts 'Current configurations for new CloudWatch Alarm {{{'
-    super(with_detail)
-    puts '}}}'
+  def put_status(indent:'', with_detail:true)
+    puts 'Current configurations for new CloudWatch Alarm'
+    super(indent:indent + '  ', with_detail: with_detail)
+    puts
   end
 end
 
@@ -356,10 +415,11 @@ class AddCloudWatchAlarmCommandShell < CommandShell
     super
   end
 
-  def put_status(with_detail=false)
-    @shell_select_instance.put_status with_detail
+  def put_status(indent:'', with_detail:false)
     puts
-    @shell_alarm_property.put_status with_detail
+    @shell_select_instance.put_status(indent: indent, with_detail: with_detail)
+    puts if with_detail
+    @shell_alarm_property.put_status(indent: indent, with_detail: with_detail)
   end
 
   def command_s
@@ -373,13 +433,17 @@ class AddCloudWatchAlarmCommandShell < CommandShell
   end
 
   def command_p
-    put_status true
+    put_status( :with_detail=>true )
     prompt false
   end
 
   def command_a
-    puts 'Apply'
-
+    metrics = @shell_select_instance.selected_metrics
+    puts '', 'Try to add %d alarms ...' % metrics.size
+    metrics.each do |m|
+      AwsClient.instance.put_metric_alarm m, @shell_alarm_property.config
+    end
+      
     command_q
   end
 
@@ -394,7 +458,6 @@ end
 class MainCommandShell < CommandShell
   def initialize
     @shell_add = AddCloudWatchAlarmCommandShell.new
-    @shell_delete = CommandShell.new
   end 
   
   def help_message
@@ -402,7 +465,6 @@ class MainCommandShell < CommandShell
 Commands
   [A]dd CloudWatch alarm by current settings
   [E]nable/disable CloudWatch alarms
-  [D]elete CloudWatch alarms
 
   [Q]uit
 '
@@ -413,12 +475,7 @@ Commands
   end
 
   def command_e
-    puts "Not yet supported"
-  end
-  
-  def command_d
-    puts "Not yet supported"
-    #@shell_delete.ask
+    puts 'Not yet supported'
   end
 
   def command_q
